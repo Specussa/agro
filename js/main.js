affd040388eca013420e04af2344e826a7528429
Spec Ussa <serega.ussa@gmail.com>
2023-07-13 15:38:07 +0300

    company section

--- js/main.js
+++ js/main.js
@@ -22,99 +22,6 @@ document.querySelector('.header__burger').addEventListener('click', function() {
 })
 // end navbar
 
-// start select
-const SELECT = '[data-select]'
-const SELECT_LIST = '[data-select-list]'
-const SELECT_ARROW = '[data-select-arrow]'
-const SELECT_ACTION = '[data-select-action]'
-const SELECT_TITLE = '[data-select-title]'
-const SELECT_INPUT = '[data-select-input]'
-const SELECT_ITEM = 'selectItem'
-const OPEN_SELECT = 'selectOpen'
-
-class Select {
-  static attach() {
-    document.querySelectorAll(SELECT)
-      .forEach(select => new Select().init(select))
-  }
-
-  init(select) {
-    if (this.findSelect(select)) {
-      this.applyListener()
-    }
-  }
-
-  applyListener() {
-    document.querySelector('*').addEventListener('click', e => {
-      const element = this.select.contains(e.target) && e.target.closest(SELECT_ACTION)
-
-      if (this.isCallSelectElement(element)) {
-        if (this.isOpened()) {
-          this.closeSelectList();
-        } else {
-          this.openSelectList()
-        }
-      }
-
-      if (this.isCallSelectItemElement(element)) {
-        this.addSelectedValue(element)
-      }
-
-      if (this.isCallSelectElement(element) !== true && this.selectOverlayIsClickedElement(element) !== true) {
-        this.closeSelectList()
-      }
-    })
-  }
-
-  isCallSelectElement(element, target) {
-    return element && OPEN_SELECT in element.dataset
-  }
-
-  isCallSelectItemElement(element, target) {
-    return element && SELECT_ITEM in element.dataset
-  }
-
-  findSelect(select) {
-
-    if (select) {
-      this.select = select
-      this.selectList = this.select.querySelector(SELECT_LIST)
-      this.selectArrow = this.select.querySelector(SELECT_ARROW)
-      this.selectTitle = this.select.querySelector(SELECT_TITLE)
-      this.selectInput = this.select.querySelector(SELECT_INPUT)
-      return true
-    }
-    return false
-  }
-
-  isOpened() {
-    return this.selectList.classList.contains('header__select_list_opened')
-  }
-
-  openSelectList() {
-    this.selectList.classList.add('header__select_list_opened')
-    this.selectArrow.classList.add('header__select_arrow_rotate')
-  }
-
-  closeSelectList() {
-    this.selectList.classList.remove('header__select_list_opened')
-    this.selectArrow.classList.remove('header__select_arrow_rotate')
-  }
-
-  addSelectedValue(element) {
-    this.selectTitle.innerHTML = element.innerHTML;
-    this.selectInput.value = element.innerHTML;
-    this.selectInput.setAttribute('value', this.selectInput.value);
-  }
-
-  selectOverlayIsClickedElement(element, target) {
-    return element && 'select' in element.dataset
-  }
-}
-
-Select.attach()
-// end select
-
 // start year
 const year = document.querySelector('.footer__year');
 if(!year){} else {
@@ -127,10 +34,11 @@ year.remove();
 // start hero slider
 const heroslider = document.querySelectorAll('.hero');
 if(!heroslider){} else {
-var interleaveOffset = 0.5;
-  const sliderSelector = '.swiper-container',
+  var interleaveOffset = 0.5;
+  const sliderSelector = '.hero__slider',
   options = {
     loop: true,
+    loopedSlides: 2,
     speed: 1000,
     parallax: true,
     autoplay: {
@@ -139,13 +47,13 @@ var interleaveOffset = 0.5;
     },
     watchSlidesProgress: true,
     pagination: {
-        el: '.swiper-pagination',
+        el: '.hero__pagination',
         clickable: true,
     },
 
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.hero__next',
+        prevEl: '.hero__prev',
     },
 
     on: {
@@ -155,7 +63,7 @@ var interleaveOffset = 0.5;
                 var slideProgress = swiper.slides[i].progress;
                 var innerOffset = swiper.width * interleaveOffset;
                 var innerTranslate = slideProgress * innerOffset;
-                swiper.slides[i].querySelector(".slide-inner").style.transform =
+                swiper.slides[i].querySelector(".hero__block").style.transform =
                 "translate3d(" + innerTranslate + "px, 0, 0)";
             }      
         },
@@ -171,7 +79,7 @@ var interleaveOffset = 0.5;
             var swiper = this;
             for (var i = 0; i < swiper.slides.length; i++) {
                 swiper.slides[i].style.transition = speed + "ms";
-                swiper.slides[i].querySelector(".slide-inner").style.transition =
+                swiper.slides[i].querySelector(".hero__block").style.transition =
                 speed + "ms";
             }
         }
@@ -180,10 +88,91 @@ var interleaveOffset = 0.5;
   const mySwiper = new Swiper(sliderSelector, options);
   mySwiper.init();
   
-  [].forEach.call(document.querySelectorAll('.slide-bg-image'), (element) => {
+  [].forEach.call(document.querySelectorAll('.hero__block'), (element) => {
     if (element.getAttribute('data-background')) {
       element.style.setProperty('background-image', 'url(' + element.getAttribute('data-background') + ')');
     }
   });
 }
 // end hero slider
+
+// start company slider
+const companyslider = document.querySelectorAll('.company__slider');
+if(!companyslider){} else {
+  var interleaveOffset = 0.5;
+  var galleryTop = new Swiper('.company__top', {
+    loop: false,
+    speed: 1000,
+    slidesPerView: '1',
+    loop: true,
+    loopedSlides: 2,
+    parallax: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: '.company__next',
+        prevEl: '.company__prev',
+    },
+
+    on: {
+        progress: function() {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+                var slideProgress = swiper.slides[i].progress;
+                var innerOffset = swiper.width * interleaveOffset;
+                var innerTranslate = slideProgress * innerOffset;
+                swiper.slides[i].querySelector(".company__block").style.transform =
+                "translate3d(" + innerTranslate + "px, 0, 0)";
+            }      
+        },
+
+        touchStart: function() {
+          var swiper = this;
+          for (var i = 0; i < swiper.slides.length; i++) {
+            swiper.slides[i].style.transition = "";
+          }
+        },
+
+        setTransition: function(speed) {
+            var swiper = this;
+            for (var i = 0; i < swiper.slides.length; i++) {
+                swiper.slides[i].style.transition = speed + "ms";
+                swiper.slides[i].querySelector(".company__block").style.transition =
+                speed + "ms";
+            }
+        }
+    }
+  });
+  
+  [].forEach.call(document.querySelectorAll('.company__block'), (element) => {
+    if (element.getAttribute('data-background')) {
+      element.style.setProperty('background-image', 'url(' + element.getAttribute('data-background') + ')');
+    }
+  });
+
+  var galleryThumbs = new Swiper('.company__thumbs', {
+    loop: false,
+    spaceBetween: 40,
+    slidesPerView: 'auto',
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+    loop: true,
+    loopedSlides: 2,
+    speed: 1000,
+    allowTouchMove: true,
+  });
+
+  var galleryText = new Swiper('.company__texts', {
+    loop: false,
+    spaceBetween: 40,
+    slidesPerView: '1',
+    touchRatio: 0.2,
+    loop: true,
+    loopedSlides: 2,
+    speed: 1000,
+    allowTouchMove: false,
+  });
+  galleryTop.controller.control = galleryThumbs && galleryText;
+  galleryThumbs.controller.control = galleryTop && galleryText;
+  galleryText.controller.control = galleryTop && galleryThumbs;
+}
+// end company slider
